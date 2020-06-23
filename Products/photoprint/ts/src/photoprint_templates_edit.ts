55dbb242ee45a43e60a145c0343f00ab33220d4d
Benoît Pin <benoit.pin@gmail.com>
2020-06-23 11:34:10 +0200

    On ajoute des boutons.

--- Products/photoprint/ts/src/photoprint_templates_edit.ts
+++ Products/photoprint/ts/src/photoprint_templates_edit.ts
@@ -42,89 +42,80 @@ class PrintOptionsEditor {
     }
 
     private initFormats(formats: Array<Format>) {
-        const formatsEnterSel =
-            d3.select(this.formatsWrapper)
-                .append('div')
-                .selectAll('div')
-                .data(formats)
-                .enter()
-                .append('div')
-                .html((d) => {
-                    let lbl= '';
-                    for (let [lang, value] of Object.entries(d.label)) {
-                        lbl += `<li><span>${value}</span>@<span>${lang}</span></li>`
-                    }
-                    return `
-                    <table class="TwoColumnForm">
-                      <tr>
-                        <th>${_("Reference")}</th>
-                        <td data-name="reference">${d.reference}</td>
-                      </tr>
-                      <tr>
-                        <th>${_("Label")}</th>
-                        <td>
-                          <ul style="list-style: none">${lbl}</ul>
-                        </td>
-                      </tr>
-                      <tr>
-                        <th>${_("Short edge")}</th>
-                        <td>
-                          <span data-name="short_edge">${d.short_edge}</span> cm
-                        </td>
-                      </tr>
-                      <tr>
-                        <th>${_("Long edge")}</th>
-                        <td>
-                          <span data-name="long_edge">${d.long_edge}</span> cm
-                        </td>
-                      </tr>
-                      <tr>
-                        <th>${_("Copies")}</th>
-                        <td>
-                          <span data-name="copies">${d.copies}</span>
-                        </td>
-                      </tr>
-                    </table>
-                    `
-                    }
-                )
+        d3.select(this.formatsWrapper)
+            // .append('div')
+            .selectAll('div')
+            .data(formats)
+            .enter()
+            .append('div')
+            .html((d, i, g) => {
+                console.log(d, i, g);
+                let lbl= '';
+                for (let [lang, value] of Object.entries(d.label)) {
+                    lbl += `<li><span>${value}</span>@<span>${lang}</span></li>`
+                }
+                return `
+                <table class="TwoColumnForm">
+                  <tr>
+                    <td colspan="2" style="text-align: right">
+                      <a href="#" title="${_("Edit")}">
+                        <i class="fas fa-edit"></i>
+                      </a>
+                      <a href="#" title="${_("Delete")}">
+                        <i class="fas fa-backspace"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>${_("Reference")}</th>
+                    <td data-name="reference">${d.reference}</td>
+                  </tr>
+                  <tr>
+                    <th>${_("Label")}</th>
+                    <td>
+                      <ul style="list-style: none">${lbl}</ul>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>${_("Short edge")}</th>
+                    <td>
+                      <span data-name="short_edge">${d.short_edge}</span> cm
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>${_("Long edge")}</th>
+                    <td>
+                      <span data-name="long_edge">${d.long_edge}</span> cm
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>${_("Copies")}</th>
+                    <td>
+                      <span data-name="copies">${d.copies}</span>
+                    </td>
+                  </tr>
+                </table>
+                `
+                }
+            )
         ;
-
-        //         .append('div')
-        //     .call(function () {
-        //         console.log(this);
-        //         console.log(arguments);
-        //     })
-        // ;
+        d3.select(this.formatsWrapper)
+            .append('div')
+            .style('text-align', 'center')
+            .append('a')
+            .on('click', () => this.createFormat())
+            .attr('href', '#')
+            .attr('title', _("Add new format…"))
+            .append('i')
+            .attr('class', 'fas fa-plus')
+        ;
     }
 
-    // private createFormat() {
-    //     let params: FormData = new FormData();
-    //     params.append('name', 'format');
-    //     params.append('indent:int', '2');
-    //     let url = `${this.absUrl}/printingOptions/printoffer/getTemplate`;
-    //     d3.json(
-    //         url,
-    //         {
-    //             body: params,
-    //             method: 'POST'}
-    //         ).then((format:Format)=>this.createNewFormatUI(format));
-    // }
-    //
-    // private createNewFormatUI(format:Format) {
-    //     const fmtBaseCell = d3.select<HTMLElement, null>(this.cells.nodes()[0]);
-    //     fmtBaseCell.append('div')
-    //         .datum(format)
-    //         .html((d) =>
-    //             `
-    //             Référence : <input type="text" name="reference"/><br/>
-    //             Libellé : <input type="text" name="label"/>
-    //             bord court : <input type="text" name="short_edge" value="${d.short_edge}"/>cm<br/>
-    //             bord long : <input type="text" name="" value="${d.long_edge}"/>cm
-    //             `
-    //         )
-    //     ;
-    // }
+    private createFormat() {
+        d3.event.stopPropagation();
+        d3.event.preventDefault();
+        console.log('Ajouter !');
+    }
 }
 
 function main() {
