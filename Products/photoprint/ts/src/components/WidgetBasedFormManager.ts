e6af4cb44e99d9d8362d5eca7cab7343f67098bb
Benoît Pin <benoit.pin@gmail.com>
2020-08-21 10:21:35 +0200

    Retrait code obsolète.

--- Products/photoprint/ts/src/components/WidgetBasedFormManager.ts
+++
@@ -1,7 +0,0 @@
-export class WidgetBasedFormManager {
-    private editArea: HTMLDivElement;
-    constructor(editingAreaSelector: string) {
-        this.editArea = document.querySelector(editingAreaSelector);
-        console.log(this.editArea);
-    }
-}
