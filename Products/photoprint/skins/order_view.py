c440ef2cb065b3673102c67997ddd0fb78dff6e7
Benoît Pin <benoit.pin@gmail.com>
2020-09-29 00:45:58 +0200

    Tabs => spaces.

--- Products/photoprint/skins/order_view.py
+++ Products/photoprint/skins/order_view.py
@@ -2,6 +2,7 @@
 from Products.CMFCore.utils import getToolByName
 from Products.photoprint.price import Price
 from Products.photoprint.cart import PrintCart
+
 uidh = getToolByName(context, 'portal_uidhandler')
 wtool = getToolByName(context, 'portal_workflow')
 
@@ -23,34 +24,34 @@ if toBePaid :
     options['checkout'] = context.ppSetExpressCheckout()
 
 if cart.locked and \
-	cart.pendingOrderPath == context.getPhysicalPath() :
-	options['orderIsCart'] = True
-	if wfstate != 'recorded' :
-		cart = PrintCart()
-		session['cart'] = cart
+cart.pendingOrderPath == context.getPhysicalPath() :
+    options['orderIsCart'] = True
+    if wfstate != 'recorded' :
+        cart = PrintCart()
+        session['cart'] = cart
 else :
-	options['orderIsCart'] = False
+    options['orderIsCart'] = False
 
 for item in context.items :
-	d = {'title'		: item['title']
-		,'description'	: item['description']
-		,'unit_price'	: item['unit_price']
-		,'quantity'		: item['quantity']
-		,'total'		: item['unit_price'] * item['quantity']
-		}
+    d = {'title' : item['title']
+        , 'description' : item['description']
+        , 'unit_price' : item['unit_price']
+        , 'quantity' : item['quantity']
+        , 'total' : item['unit_price'] * item['quantity']
+         }
 
-	b = uidh.queryBrain(item['cmf_uid'])
-	if b :
-		size = b.getThumbnailSize
-		thumbInfo = {'thumbUrl'		: '%s/getThumbnail' % b.getURL()
-					,'thumbHeight'	: size['height'] / 2
-					,'thumbWidth'	: size['width'] / 2
-					,'alt'			: ('%s - %s' % (b.Title, b.Description)).strip(' -')
-					}
-		d.update(thumbInfo)
-	quantity += d['quantity']
-	prices.append(d['total'])
-	infos.append(d)
+    b = uidh.queryBrain(item['cmf_uid'])
+    if b :
+        size = b.getThumbnailSize
+        thumbInfo = {'thumbUrl' : '%s/getThumbnail' % b.getURL()
+            , 'thumbHeight' : size['height'] / 2
+            , 'thumbWidth' : size['width'] / 2
+            , 'alt' : ('%s - %s' % (b.Title, b.Description)).strip(' -')
+                     }
+        d.update(thumbInfo)
+    quantity += d['quantity']
+    prices.append(d['total'])
+    infos.append(d)
 
 options['infos'] = infos
 options['quantity'] = quantity
